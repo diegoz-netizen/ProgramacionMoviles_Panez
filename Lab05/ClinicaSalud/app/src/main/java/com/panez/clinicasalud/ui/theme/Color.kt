cae2df40a527311bb17d27f7ce47a73a2c118f6d
diegoz-netizen <diego.panez@tecsup.edu.pe>
2026-09-22 20:47:20 -0500

    style: tema morado de la clinica y paleta de colores

--- Lab05/ClinicaSalud/app/src/main/java/com/panez/clinicasalud/ui/theme/Color.kt
+++ Lab05/ClinicaSalud/app/src/main/java/com/panez/clinicasalud/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package com.panez.clinicasalud.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val MoradoPrincipal = Color(0xFF5B2C8D)
+val MoradoClaro = Color(0xFFE8DDF5)
+val MoradoOscuro = Color(0xFF3E1D63)
+val VerdeEstado = Color(0xFF4CAF50)
+val GrisEstado = Color(0xFFBDBDBD)
+val GrisFondo = Color(0xFFF5F5F5)
+val GrisTexto = Color(0xFF757575)
